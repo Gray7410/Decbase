2c1622a49efb9f4307d04c37947e41febe69f332
Gray7410 <gray7410@yandex.ru>
2021-01-07 23:43:14 +0500

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function () {
 	$('.service-slider__list').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		prevArrow: '<a class="service-slider__arrow service-slider__arrow--left" href="#"></a>',
 		nextArrow: '<a class="service-slider__arrow service-slider__arrow--right" href="#"></a>',
-		centerMode: true,
-		responsive:[
+		// centerMode: true,
+		responsive: [
 			{
 				breakpoint: 1500,
 				settings: {
@@ -20,12 +20,42 @@ $(function(){
 			}
 		]
 	});
+
+});
+
+$(document).ready(function () {
+	var a = 0;
+	$(window).scroll(function () {
+		var oTop = $('#counter').offset().top - window.innerHeight;
+		if (a == 0 && $(window).scrollTop() > oTop) {
+			$('.count').each(function () {
+				var $this = $(this),
+					countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+					{
+						duration: 1500,
+						easing: 'swing',
+						step: function () {
+							$this.text(Math.floor(this.countNum));
+						},
+						complete: function () {
+							$this.text(this.countNum);
+						}
+					});
+			});
+			a = 1;
+		}
+	});
 });
 
 const menuToggle = document.querySelector("#menu-toggle");
 const mobileNav = document.querySelector("#mobile-nav");
 
-menuToggle.onclick = function() {
+menuToggle.onclick = function () {
 	menuToggle.classList.toggle('header__burger--active');
 	mobileNav.classList.toggle('mobile-nav--active');
 }
