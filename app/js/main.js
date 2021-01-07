719a1bd8e105288ea650ed04da0e9161b204f016
Gray7410 <gray7410@yandex.ru>
2021-01-07 19:45:12 +0500

    slider-adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,21 @@ $(function(){
 		slidesToScroll: 1,
 		prevArrow: '<a class="service-slider__arrow service-slider__arrow--left" href="#"></a>',
 		nextArrow: '<a class="service-slider__arrow service-slider__arrow--right" href="#"></a>',
+		centerMode: true,
+		responsive:[
+			{
+				breakpoint: 1500,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 1050,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+		]
 	});
 });
 
